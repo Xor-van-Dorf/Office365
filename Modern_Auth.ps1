e2fc15aead703a184ff20db696c8d0d1c91fe619
Xor-van-Dorf <116817870+Xor-van-Dorf@users.noreply.github.com>
2022-10-27 14:30:12 -0400

    Update Modern_Auth.ps1

--- Modern_Auth.ps1
+++ Modern_Auth.ps1
@@ -1,5 +1,33 @@
 #For Office 2013/2016, force modern auth
 
-New-ItemProperty -Path "HKLM:\HKEY_CURRENT_USER\Software\Microsoft\Exchange\" -Name AlwaysUseMSOAuthForAutoDiscover -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path "HKLM:\HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\Identity\" -Name EnableADAL -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path "HKLM:\HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\Identity\" -Name Version -Value 1 -PropertyType DWORD -Force
+$registryPath1 = "HKCU:\Software\Microsoft\Exchange"
+$registryPath2 = "HKCU:\Software\Microsoft\Office\15.0\Common\Identity"
+$value = "1"
+$name1 = "AlwaysUseMSOAuthForAutoDiscover"
+$name2 = "EnableADAL"
+$name3 = "Version"
+
+
+IF(!(Test-Path $registryPath1))
+    {
+    New-Item -Path $registryPath1 -Force | Out-Null
+    New-ItemProperty -Path $registryPath1 -Name $name1 -Value $value -PropertyType DWORD -Force | Out-Null
+    }
+
+ ELSE 
+    {
+    New-ItemProperty -Path $registryPath1 -Name $name1 -Value $value -PropertyType DWORD -Force | Out-Null
+    }
+
+IF(!(Test-Path $registryPath2))
+    {
+    New-Item -Path $registryPath2 -Force | Out-Null
+    New-ItemProperty -Path $registryPath2 -Name $name2 -Value $value -PropertyType DWORD -Force | Out-Null
+    New-ItemProperty -Path $registryPath2 -Name $name3 -Value $value -PropertyType DWORD -Force | Out-Null
+    }
+
+ ELSE 
+    {
+    New-ItemProperty -Path $registryPath2 -Name $name2 -Value $value -PropertyType DWORD -Force | Out-Null
+    New-ItemProperty -Path $registryPath2 -Name $name3 -Value $value -PropertyType DWORD -Force | Out-Null
+    }
