9a528f586b41ae93b41d873fb813afda9fd57597
Xor-van-Dorf <116817870+Xor-van-Dorf@users.noreply.github.com>
2022-10-27 11:59:40 -0400

    Convert 365 mailbox to shared mailbox and remove licenses

---
+++ Convert365MailboxToShared.ps1
@@ -0,0 +1,22 @@
+#Converts an Office 365 Mailbox from a standard one to a shared one and removes the licenses.
+
+Connect-ExchangeOnline
+
+$usermailbox = read-Host 'User mailbox to convert to shared mailbox'
+$supervisor = read-Host 'User who is going to be having access to shared mailbox'
+
+Set-Mailbox $usermailbox -Type shared
+
+
+#Sleep a little for Microsoft
+start-sleep -s 90
+
+
+##Adjust these licenses according to the license that you have, but these two
+##are the most common ones.
+
+
+Add-MailboxPermission -Identity $usermailbox -User $supervisor -AccessRights FullAccess
+connect-msolservice 
+Set-MsolUserLicense -UserPrincipalName "$usermailbox" -RemoveLicenses YOURO365NAME:EXCHANGESTANDARD
+Set-MsolUserLicense -UserPrincipalName "$usermailbox" -RemoveLicenses YOURO365NAME:O365_BUSINESS
