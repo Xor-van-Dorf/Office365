addad49c17aca60a190649d6a83c42c8b9082978
Xor-van-Dorf <116817870+Xor-van-Dorf@users.noreply.github.com>
2022-10-27 14:42:22 -0400

    Disable automapping

--- Automapping.ps1
+++ Automapping.ps1
@@ -1,2 +1,4 @@
 #Automapping disabled
-Add-MailboxPermission -Identity shared mailbox -User user -AccessRights FullAccess -AutoMapping:$false
+$shared = Read-Host "Enter shared mailbox"
+$user = Read-Host "Enter user mailbox"
+Add-MailboxPermission -Identity $shared -User $user -AccessRights FullAccess -AutoMapping:$false
